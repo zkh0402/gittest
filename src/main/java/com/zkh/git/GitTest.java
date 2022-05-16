bbd383831d6af24b74c156b335f88ae0d941b54a
zkh <904014115@qq.com>
2022-05-16 16:53:53 +0800

    first commit

---
+++ src/main/java/com/zkh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zkh.git;
+
+/**
+ * @author zkh
+ * @date 2022/5/16 -16:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
